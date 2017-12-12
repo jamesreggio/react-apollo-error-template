3151ce058904dd8955f1e8b222b9eea1a5ed7b2b
James Reggio <james.reggio@gmail.com>
2017-12-12 15:44:29 -0500

    Test apollo-cache-persist

--- src/index.js
+++ src/index.js
@@ -5,12 +5,21 @@ import { render } from "react-dom";
 import { ApolloClient } from "apollo-client";
 import { ApolloProvider } from "react-apollo";
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { persistCache } from "apollo-cache-persist";
 
 import { link } from "./graphql/link";
 import App from "./App";
 
+const cache = new InMemoryCache();
+
+persistCache({
+  cache,
+  storage: window.localStorage,
+  debug: true,
+});
+
 const client = new ApolloClient({
-  cache: new InMemoryCache(),
+  cache,
   link,
 });
 
